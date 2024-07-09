a85b349cda16719965a9d19f0d80b33571f7bbc7
정지민 <108014449+stopmin@users.noreply.github.com>
2024-07-09 18:20:25 +0900

    각종 util(JWT, CommonResponse, GlobalException handler)에 관한 코드 생성 (#8)
    * ignore application-secret.yml
    
    * global error exception handler
    
    * JWT관련 종속성 추가
    
    * Common Response 정의
    
    * JWT Util 정의
    
    * ErrorCode 코드 수정
    
    * application-secret 활성화
    
    * user 프로토타입 생성

---
+++ src/main/java/gyeongdan/util/ErrorCode.java
@@ -0,0 +1,25 @@
+package gyeongdan.util;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    // User ErrorMessage
+    LOGIN_FAILED("로그인에 실패하였습니다.", HttpStatus.UNAUTHORIZED),
+    USER_NOT_FOUND("유저를 찾을 수 없습니다.", HttpStatus.UNAUTHORIZED);
+
+    private final String message;
+    private final HttpStatus status;
+
+    ErrorCode(String message, HttpStatus status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
