a3f222b209dbe818b9a5eaeebfafa3a9a9c2a359
stopmin <geemin0@naver.com>
2024-07-11 03:38:23 +0900

    기사 관리자 API (승인 및 수정)

--- src/main/java/gyeongdan/article/controller/ArticleController.java
+++ src/main/java/gyeongdan/article/controller/ArticleController.java
@@ -2,6 +2,7 @@ package gyeongdan.article.controller;
 
 import gyeongdan.article.domain.Article;
 import gyeongdan.article.dto.ArticleCreateRequest;
+import gyeongdan.article.dto.ArticleUpdateRequest;
 import gyeongdan.article.service.ArticleService;
 import gyeongdan.util.CommonResponse;
 import java.util.List;
@@ -15,30 +16,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("/api")
+@RequestMapping("/api/article")
 @RequiredArgsConstructor
 public class ArticleController {
 
     private final ArticleService articleService;
 
-    @GetMapping("/article")
+    @GetMapping("")
     public ResponseEntity<?> getArticle(@RequestParam Long id) {
         Article article = articleService.getValidArticle(id);
         return ResponseEntity.ok(new CommonResponse<>(article, "게시글 조회 성공", true));
     }
 
-    @GetMapping("/articles")
+    @GetMapping("")
     public ResponseEntity<?> getArticles() {
         List<Article> article = articleService.getArticles();
         return ResponseEntity.ok(new CommonResponse<>(article, "게시글 조회 성공", true));
     }
 
-    // TODO: 매니저 권한이 필요한 API
-    @PostMapping("/article")
-    public ResponseEntity<?> saveArticle(@RequestBody ArticleCreateRequest article) {
-        Article savedArticle = articleService.saveArticle(article);
+    // TODO: 매니저 인가 필요
+    @PostMapping("/validate")
+    public ResponseEntity<?> saveArticle(@RequestParam Long id) {
+        Article savedArticle = articleService.validateArticle(id);
         return ResponseEntity.ok(new CommonResponse<>(savedArticle, "게시글 저장 성공", true));
     }
 
+
+    @PostMapping("/update")
+    public ResponseEntity<?> updateArticle(@RequestBody ArticleUpdateRequest articleUpdateRequest) {
+        Long savedArticleId = articleService.updateArticle(articleUpdateRequest);
+        return ResponseEntity.ok(new CommonResponse<>(savedArticleId, "게시글 수정 성공", true));
+    }
 }
 
