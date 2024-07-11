c6d4855b15302d8fd42e9284d08be3f3f5630f87
stopmin <geemin0@naver.com>
2024-07-11 14:59:16 +0900

    롬복 적용

--- src/main/java/gyeongdan/user/controller/KakaoOauthController.java
+++ src/main/java/gyeongdan/user/controller/KakaoOauthController.java
@@ -2,19 +2,17 @@ package gyeongdan.user.controller;
 
 import gyeongdan.user.service.KakaoOauthService;
 import gyeongdan.util.CommonResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class KakaoOauthController {
 
     private final KakaoOauthService kakaoOauthService;
-
-    public KakaoOauthController(KakaoOauthService kakaoOauthService) {
-        this.kakaoOauthService = kakaoOauthService;
-    }
 
     @GetMapping("/login")
     public ResponseEntity<?> loginForm() {
