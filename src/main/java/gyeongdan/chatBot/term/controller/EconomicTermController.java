addbc0374d817352ffae5790428358c598bc098c
pykido <animoto1@naver.com>
2024-07-14 02:20:00 +0900

    feat : 경단어 응답 고도화

--- src/main/java/gyeongdan/chatBot/term/controller/EconomicTermController.java
+++ src/main/java/gyeongdan/chatBot/term/controller/EconomicTermController.java
@@ -1,11 +1,11 @@
 package gyeongdan.chatBot.term.controller;
 
 import gyeongdan.chatBot.term.service.EconomicTermService;
+import gyeongdan.util.CommonResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -15,7 +15,7 @@ public class EconomicTermController {
     private final EconomicTermService economicTermService;
 
     @GetMapping("/search")
-    public String search(@RequestParam String question) throws Exception {
+    public CommonResponse<Map<String, String>> search(@RequestBody String question) throws Exception {
         return economicTermService.findEconomicTerm(question);
     }
 }
